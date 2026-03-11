e48a0871987a3bf3cef0c4ff614b3e284a19376f
k1r1kato <k1r1kato47@gmail.com>
2026-03-11 14:04:05 +0600

    Fix models

---
+++ CreateTaskRequest.cs
@@ -0,0 +1,10 @@
+public class CreateTaskRequest
+{
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public int? Priority { get; set; }
+
+    public bool? IsCompleted { get; set; }
+}
