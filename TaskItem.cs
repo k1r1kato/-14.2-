30836b55b84c95388e877b150fcf993890e54873
k1r1kato <k1r1kato47@gmail.com>
2026-03-11 14:11:32 +0600

    Update TaskItem model

---
+++ TaskItem.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = "";
+
+    public string? Description { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public int Priority { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
